0a74bc47b1b0e69bee86bff81836facd82d8fce2
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-15 22:05:47 -0500

    feat(models): add TipoTramite entity with attributes and repository interface

---
+++ backend/demo/src/main/java/com/citcd/demo/catalogos/tipotramite/models/TipoTramite.java
@@ -0,0 +1,43 @@
+package com.citcd.demo.catalogos.tipotramite.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Data;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "tipo_tramite_codigo_key", columnNames = {
+        "codigo" }))
+@Data
+public class TipoTramite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String codigo;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Boolean esActivo;
+
+    private LocalDate creadoEn;
+
+    private LocalDate actualizadoEn;
+
+    public boolean esActivo() {
+        return this.esActivo;
+    }
+
+}
---
+++ backend/demo/src/main/java/com/citcd/demo/catalogos/tipotramite/repositories/TipoTramiteRepository.java
@@ -0,0 +1,9 @@
+package com.citcd.demo.catalogos.tipotramite.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.citcd.demo.catalogos.tipotramite.models.TipoTramite;
+
+public interface TipoTramiteRepository extends JpaRepository<TipoTramite, Long> {
+
+}
