cb8133e96cbc9d371e3aa42b819080d23f1bce11
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-16 16:31:53 -0500

    feat(usuario): add method to find Usuario by ID and RolUsuario

--- backend/demo/src/main/java/com/citcd/demo/auth/repositories/UsuarioRepository.java
+++ backend/demo/src/main/java/com/citcd/demo/auth/repositories/UsuarioRepository.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.citcd.demo.auth.model.Usuario;
+import com.citcd.demo.auth.model.enums.RolUsuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByIdAndRol(Long id, RolUsuario rol);
 
 }
