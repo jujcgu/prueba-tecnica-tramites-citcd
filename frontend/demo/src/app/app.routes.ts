8097aad0a1e14f743f60825029fb0d82afc85e44
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-17 17:17:33 -0500

    feat: add navigation styles and home page

--- frontend/demo/src/app/app.routes.ts
+++ frontend/demo/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Navigation } from './layout/navigation/navigation';
+import { HomePage } from './features/home/home-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: Navigation,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home-page').then((m) => m.HomePage),
+      },
+    ],
   },
 ];
--- frontend/demo/src/app/layout/navigation/navigation.ts
+++ frontend/demo/src/app/layout/navigation/navigation.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -20,14 +21,15 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
-  ]
+    RouterOutlet,
+    RouterLink
+  ],
 })
 export class Navigation {
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  );
 }
