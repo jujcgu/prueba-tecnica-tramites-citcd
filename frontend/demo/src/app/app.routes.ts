a4d26e8f77b7c210d51d130f754287cb73abf8b0
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-17 20:23:43 -0500

    feat: guard routes

--- frontend/demo/src/app/app.routes.ts
+++ frontend/demo/src/app/app.routes.ts
@@ -1,16 +1,25 @@
 import { Routes } from '@angular/router';
 import { Navigation } from './layout/navigation/navigation';
-import { HomePage } from './features/home/home-page';
+import { authGuard } from './core/auth/guards/auth-guard';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login-page').then((m) => m.LoginPage),
+  },
+
+  {
     path: '',
-    component: Navigation,
+    loadComponent: () => import('./layout/navigation/navigation').then((m) => m.Navigation),
+    canActivate: [authGuard],
     children: [
       {
         path: 'home',
         loadComponent: () => import('./features/home/home-page').then((m) => m.HomePage),
       },
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
     ],
   },
+
+  { path: '**', redirectTo: '' },
 ];
